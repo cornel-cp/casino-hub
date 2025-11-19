ea1fcc7e6dd0f058f6a3066e5dc5967c37ef39be
cornel <mufasa030831@gmail.com>
2025-11-20 05:48:38 +0900

    Update: user avatar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
 
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        dark:{
+          100: '#cbd7ff08',
+          200: '#1a1d29',
+          300: '#0f111a8c',
+        }
+      },
     },
   },
   plugins: [require("@tailwindcss/container-queries")],
